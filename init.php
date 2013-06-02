929fd79fb71f43e241db9facf2709c9d27aeb04c
Frederik Wordenskjold <frederik@wordenskjold.dk>
2013-06-02 13:38:47 +0200

    Update init.php

--- init.php
+++ init.php
@@ -5,7 +5,10 @@ function xhp_register_library(){
 }
 
 function __xhp_autoload($class_name) {
-  require_once('php-lib/' . $class_name . '.php');
+
+  if(file_exists('php-lib/' . $class_name . '.php')){
+		include_once('php-lib/' . $class_name . '.php');
+	}
 }
 
 spl_autoload_register('__xhp_autoload');
