f9c8a8f3d348addfcccf258c6f354ef57ec64178
Frederik Wordenskjold <frederik@wordenskjold.dk>
2013-06-02 16:24:11 +0300

    Update init.php

--- init.php
+++ init.php
@@ -1,15 +1,47 @@
 <?php
 
-function xhp_register_library(){
-  
-}
+class XHP_Loader{
 
-function __xhp_autoload($class_name) {
+	private $classMap = array();
+	private static $instance = null;
 
-  if(file_exists('php-lib/' . $class_name . '.php')){
-		include_once('php-lib/' . $class_name . '.php');
+	private function __construct(){
+		$this->registerMap();
+		include_once('php-lib/init.php');
+	}
+
+	private function registerMap(){
+		$this->classMap['ci'] = 'codeigniter.php';
+		$this->classMap['ui'] = 'common.php';
+	}
+
+	static function instance(){
+        if (!isset(static::$instance)) {
+            static::$instance = new static;
+        }
+		return static::$instance;
+	}
+
+	public function getClassMap(){
+		return $this->classMap;
+	}
+
+	static function load($class){
+		$loader = XHP_Loader::instance();
+		$frag = explode('__', $class);
+
+		if(strstr($frag[0], "xhp_") != false){
+			$prefix = explode('_', $frag[0])[1];
+			$map = $loader->getClassMap();
+			if(isset($map[$prefix])){
+				include_once('php-lib/' . $loader->getClassMap()[$prefix]);
+			}
+		}
 	}
 }
 
+function __xhp_autoload($class_name) {
+	XHP_Loader::load($class_name);
+}
+
 spl_autoload_register('__xhp_autoload');
-xhp_register_library();
